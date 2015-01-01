c7a095be31c882e30654ab59329cd5a1725d3a04
Eduardo Eidelwein Berlitz <eberlitz@gmail.com>
2015-01-01 21:02:38 -0200

    adjusted files names and paths

--- index.js
+++ index.js
@@ -124,88 +124,46 @@ var getPlatformSplashs = function() {
     platforms.push({
         name: 'ios',
         isAdded: true,
-        splashPath: 'res/ios/',
-        splash: [{
-            name: 'Default-568h@2x~iphone.png',
-            width: 640,
-            height: 1136
-        }, {
-            name: 'Default-667h.png',
-            width: 750,
-            height: 1334
-        }, {
-            name: 'Default-736h.png',
-            width: 1242,
-            height: 2208
-        }, {
-            name: 'Default-Landscape-736h.png',
-            width: 2208,
-            height: 1242
-        }, {
-            name: 'Default-Landscape@2x~ipad.png',
-            width: 2048,
-            height: 1536
-        }, {
-            name: 'Default-Landscape~ipad.png',
-            width: 1024,
-            height: 768
-        }, {
-            name: 'Default-Portrait@2x~ipad.png',
-            width: 1536,
-            height: 2048
-        }, {
-            name: 'Default-Portrait~ipad.png',
-            width: 768,
-            height: 1024
-        }, {
-            name: 'Default@2x~iphone.png',
-            width: 640,
-            height: 960
-        }, {
-            name: 'Default~iphone.png',
-            width: 320,
-            height: 480
-        }, ]
+        splashPath: 'res/screen/ios/',
+        splash: [
+            { name: 'Default~iphone.png', width: 320, height:480 },
+            { name: 'Default@2x~iphone.png', width: 640, height:960 },
+            { name: 'Default-Portrait~ipad.png', width: 768, height:1024 },
+            { name: 'Default-Portrait@2x~ipad.png', width: 1536, height:2048 },
+            { name: 'Default-Landscape~ipad.png', width: 1024, height:768 },
+            { name: 'Default-Landscape@2x~ipad.png', width: 2048, height:1536 },
+            { name: 'Default-568h@2x~iphone.png', width: 640, height:1136 },
+            { name: 'Default-667h.png', width: 750, height:1334 },
+            { name: 'Default-736h.png', width: 1242, height:2208 },
+            { name: 'Default-Landscape-736h.png', width: 2208, height:1242 },
+        ]
     });
     platforms.push({
         name: 'android',
         isAdded: true,
-        splashPath: 'res/android/',
-        splash: [{
-            name: 'drawable-land-ldpi/screen.png',
-            width: 320,
-            height: 200
-        }, {
-            name: 'drawable-land-mdpi/screen.png',
-            width: 480,
-            height: 320
-        }, {
-            name: 'drawable-land-hdpi/screen.png',
-            width: 800,
-            height: 480
-        }, {
-            name: 'drawable-land-xhdpi/screen.png',
-            width: 1280,
-            height: 720
-        }, {
-            name: 'drawable-port-ldpi/screen.png',
-            width: 200,
-            height: 320
-        }, {
-            name: 'drawable-port-mdpi/screen.png',
-            width: 320,
-            height: 480
-        }, {
-            name: 'drawable-port-hdpi/screen.png',
-            width: 480,
-            height: 800
-        }, {
-            name: 'drawable-port-xhdpi/screen.png',
-            width: 720,
-            height: 1280
-        }, ]
+        splashPath: 'res/screen/android/',
+        splash: [
+            { name: 'splash-land-ldpi.png', width: 320, height: 200 },
+            { name: 'splash-land-mdpi.png', width: 480, height: 320 },
+            { name: 'splash-land-hdpi.png', width: 800, height: 480 },
+            { name: 'splash-land-xhdpi.png', width: 1280, height: 720 },
+
+            { name: 'splash-port-ldpi.png', width: 200, height: 320 },
+            { name: 'splash-port-mdpi.png', width: 320, height: 480 },
+            { name: 'splash-port-hdpi.png', width: 480, height: 800 },
+            { name: 'splash-port-xhdpi.png', width: 720, height: 1280 }
+        ]
     });
-    // TODO: add all platforms
+
+    platforms.push({
+        name: 'wp8',
+        isAdded: true,
+        splashPath: 'res/screen/wp8/',
+        splash: [
+            { name: 'SplashScreenImage.png', width: 768, height: 1280 }
+        ]
+    });
+
     deferred.resolve(platforms);
     return deferred.promise;
 };
